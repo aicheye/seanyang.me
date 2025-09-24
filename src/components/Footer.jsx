842723c5f3c0cf8c3417b4db686a4a5dbfb1c673
Sean Yang <sean@seanyang.me>
2025-09-24 02:12:09 -0400

    update: improve layout and structure of Footer component for better responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,35 +23,45 @@ export default function Footer({ co2 = "0.03", percentage = "93", url = "" }) {
   return (
     <>
       <footer className="py-8 flex flex-col items-center gap-2 sm:gap-4 z-10">
-        <div id="contact" className="flex sm:flex-row flex-col sm:gap-4 gap-1 items-center" style={{ color: "var(--page-text)" }}>
-          <div className="flex sm:gap-4 gap-2 items-center">
-            <span className="text-4xl flex sm:gap-2 items-center">
-              <a href="https://github.com/aicheye/" aria-label="GitHub" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("github.com/aicheye")} onMouseLeave={handleMouseLeave}>
-                <GitHubIcon className="w-10 h-10" />
-              </a>
-              <a href="https://www.linkedin.com/in/syang07/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("linkedin.com/in/syang07")} onMouseLeave={handleMouseLeave}>
-                <LinkedInIcon className="w-10 h-10" />
-              </a>
-              <a href="https://www.instagram.com/seanyang_esports_gaming/" aria-label="Instagram" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("instagram.com/seanyang_esports_gaming")} onMouseLeave={handleMouseLeave}>
-                <InstagramIcon className="w-10 h-10" />
-              </a>
-              <a href="https://letterboxd.com/aicheye/" aria-label="Letterboxd" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("letterboxd.com/aicheye")} onMouseLeave={handleMouseLeave}>
-                <LetterboxdIcon className="w-10 h-10" />
-              </a>
-            </span>
-            <span>·</span>
+        <div id="contact" className="flex lg:flex-row md:flex-row flex-col lg:gap-4 md:gap-4 gap-1 items-center" style={{ color: "var(--page-text)" }}>
+          <div className="flex sm:flex-row flex-col sm:gap-4 gap-1 items-center">
+            <div className="flex sm:gap-4 gap-2 items-center">
+              <span className="text-4xl flex sm:gap-2 items-center">
+                <a href="https://github.com/aicheye/" aria-label="GitHub" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("github.com/aicheye")} onMouseLeave={handleMouseLeave}>
+                  <GitHubIcon className="w-10 h-10" />
+                </a>
+                <a href="https://www.linkedin.com/in/syang07/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("linkedin.com/in/syang07")} onMouseLeave={handleMouseLeave}>
+                  <LinkedInIcon className="w-10 h-10" />
+                </a>
+                <a href="https://www.instagram.com/seanyang_esports_gaming/" aria-label="Instagram" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("instagram.com/seanyang_esports_gaming")} onMouseLeave={handleMouseLeave}>
+                  <InstagramIcon className="w-10 h-10" />
+                </a>
+                <a href="https://letterboxd.com/aicheye/" aria-label="Letterboxd" target="_blank" rel="noopener noreferrer" onMouseEnter={() => handleMouseEnter("letterboxd.com/aicheye")} onMouseLeave={handleMouseLeave}>
+                  <LetterboxdIcon className="w-10 h-10" />
+                </a>
+              </span>
+            </div>
+            <span className="hidden sm:block">·</span>
             <a href="mailto:sean@seanyang.me" aria-label="Email" className="text-lg underline" target="_blank" rel="noopener noreferrer">
               <div className="flex items-center justify-center" style={{ height: "32px" }}>
                 sean@seanyang.me
               </div>
             </a>
           </div>
-          <span className="hidden sm:block">·</span>
-          <a href="https://docs.seanyang.me/resume/resume.pdf" target="_blank" rel="noopener noreferrer" className="underline text-lg flex items-center gap-2">
-            <div className="flex items-center justify-center gap-2" style={{ height: "32px" }}>
-              View Resumé <ExternalLinkIcon className="w-6 h-6" />
-            </div>
-          </a>
+          <span className="hidden lg:block md:block">·</span>
+          <div className="flex flex-row gap-4 items-center">
+            <a href="https://docs.seanyang.me/resume/resume.pdf" target="_blank" rel="noopener noreferrer" className="underline text-lg flex items-center gap-2">
+              <div className="flex items-center justify-center gap-2" style={{ height: "32px" }}>
+                View Resumé <ExternalLinkIcon className="w-6 h-6" />
+              </div>
+            </a>
+            <span>·</span>
+            <a href="https://docs.seanyang.me/" target="_blank" rel="noopener noreferrer" className="underline text-lg flex items-center gap-2">
+              <div className="flex items-center justify-center gap-2" style={{ height: "32px" }}>
+                Docs <ExternalLinkIcon className="w-6 h-6" />
+              </div>
+            </a>
+          </div>
         </div>
         <div className="items-center text-md" style={{ color: "var(--page-text)" }}>
           <WebsiteCarbonBadge dark={!isLightMode} co2={co2} percentage={percentage} url={"seanyang-me" + (url ? `-${url}` : "")} />
