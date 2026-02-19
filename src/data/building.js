1f4141f5f245964ed706d42bd20107388fa1e2dc
Sean Yang <sean@seanyang.me>
2026-02-19 06:15:58 -0500

    feat: remove EZP2P Arcade project from buildingProjects

--- src/data/building.js
+++ src/data/building.js
@@ -7,14 +7,6 @@ const buildingProjects = [
     description: "Rapid-inference motion prediction models for autonomous vehicles",
     hoverText: "🚗 More on Eve",
   },
-  {
-    name: "EZP2P Arcade",
-    url: "https://ezp2p.seanyang.me",
-    image: "/icons/ezp2p.png",
-    alt: "EZP2P Arcade Logo",
-    description: "A peer-to-peer gaming platform that supports multiple classic games",
-    hoverText: "🕹️ Play now",
-  },
 ];
 
 export default buildingProjects;
