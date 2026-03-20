39dc83e35f68f8539ad0a7094d30b2a80c295266
Sean Yang <sean@seanyang.me>
2026-03-20 16:39:14 -0400

    fix: adjust footer padding and enhance webrings layout; add webring navigation arrows and improve structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,9 +95,18 @@ export default function Page() {
 
         <footer>
           <a href="https://websitecarbon.com/website/seanyang-me/" target="_blank" rel="noopener noreferrer">0.02 g CO₂ / view</a>
-          <a className="webring" href="https://se-webring.xyz" target="_blank" rel="noopener noreferrer">
-            <img src="/logo_b.png" alt="SE Webring" />
-          </a>
+          <div className="webrings-wrapper">
+            <a className="webring" href="https://se-webring.xyz" target="_blank" rel="noopener noreferrer">
+              <img src="/logo_b.png" alt="SE Webring" />
+            </a>
+            <div className="webring">
+              <a className="webring-arrow" href="https://se30webring.com?from=https://seanyang.me&dir=prev">←</a>
+              <a href="https://se30webring.com" target="_blank" rel="noopener noreferrer">
+                <img src="https://se30webring.com/assets/icon_black.svg" alt="SE '30 Webring" />
+              </a>
+              <a className="webring-arrow" href="https://se30webring.com?from=https://seanyang.me&dir=next">→</a>
+            </div>
+          </div>
         </footer>
       </div>
     </>
