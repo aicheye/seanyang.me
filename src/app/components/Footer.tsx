298ee921e4a32a45a47799e65cb089c59b1afce1
Sean Yang <sean@seanyang.me>
2026-07-09 20:43:51 -0700

    Replace logo_b.png with logo_b.svg and update references; remove unused image files

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,14 +4,14 @@ export function Footer() {
   return (
     <footer>
       <a href="https://websitecarbon.com/website/seanyang-me/" target="_blank" rel="noopener noreferrer">0.03 g CO₂ / view</a>
-      <div className="webrings-wrapper">
+      <div className="webring-wrapper">
         <a className="webring" href="https://se-webring.xyz" target="_blank" rel="noopener noreferrer">
-          <Image src="/logo_b.png" alt="SE Webring" width={32} height={22} />
+          <Image src="/logo_b.svg" alt="SE Webring" width={32} height={22} />
         </a>
         <div className="webring">
           <a className="webring-arrow" href="https://se30webring.com?from=https://seanyang.me&dir=prev">←</a>
           <a href="https://se30webring.com" target="_blank" rel="noopener noreferrer">
-            <Image src="https://se30webring.com/assets/icon_black.svg" alt="SE'30 Webring" width={23} height={22} />
+            <Image src="https://se30webring.com/assets/icon_black.svg" alt="SE'30 Webring" width={24} height={22} />
           </a>
           <a className="webring-arrow" href="https://se30webring.com?from=https://seanyang.me&dir=next">→</a>
         </div>
